c1e60e8ed28a959136aef6e2b834104d559ed1f9
aaron chen <aaron.xichen@gmail.com>
2016-10-11 15:08:25 +0800

    clean up

--- test.lua
+++
@@ -1,9 +0,0 @@
-require 'nn';
-require 'loadcaffe';
-local utee = require 'utee'
-d = utee.loadTxt('bitsSetting.config')
-for k, v in pairs(d) do
-    print(k, v)
-end
-
-utee.saveTxt("bits.config", d)
